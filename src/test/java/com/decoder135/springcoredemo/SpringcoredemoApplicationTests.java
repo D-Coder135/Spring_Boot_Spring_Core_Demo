28b909c02f55a6bd55106a0d7888ccf8267abd41
D-Coder135 <devansh1352000@gmail.com>
2023-03-28 15:14:19 +0530

    Initial commit

---
+++ src/test/java/com/decoder135/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decoder135.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
