46cde02aa7fd6630b6761dd5deabc995f5ef6260
D-Coder135 <devansh1352000@gmail.com>
2023-03-28 15:23:48 +0530

    update the return statement inside the getDailyWorkout() method body

--- src/main/java/com/decoder135/springcoredemo/CricketCoach.java
+++ src/main/java/com/decoder135/springcoredemo/CricketCoach.java
@@ -3,6 +3,6 @@ package com.decoder135.springcoredemo;
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return null;
+        return "Practice fast bowling for 15 minutes daily.";
     }
 }
