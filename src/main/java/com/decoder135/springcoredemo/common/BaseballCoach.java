5f58e7c2f1cd4db3588b15a6a1bf20962fd1a416
D-Coder135 <devansh1352000@gmail.com>
2023-03-28 19:32:06 +0530

    implement the Coach interface inside the BaseballCoach.java class

--- src/main/java/com/decoder135/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/BaseballCoach.java
@@ -3,5 +3,5 @@ package com.decoder135.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 }
