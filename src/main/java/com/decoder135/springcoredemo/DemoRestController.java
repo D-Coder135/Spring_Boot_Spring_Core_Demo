4d170cc5263458c2c2d1c13aee115f893e105324
D-Coder135 <devansh1352000@gmail.com>
2023-03-28 15:28:33 +0530

    add some comments inside the code

--- src/main/java/com/decoder135/springcoredemo/DemoRestController.java
+++ src/main/java/com/decoder135/springcoredemo/DemoRestController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
+
+    // define a private field for the dependency
+
+    // define a constructor for the dependency injection
+
 }
