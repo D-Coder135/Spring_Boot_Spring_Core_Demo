2fd9a2c16ea485a6663bac1f4d091c2ae8efe6f2
D-Coder135 <devansh1352000@gmail.com>
2023-03-28 19:35:10 +0530

    update the return statement for the overridden method

--- src/main/java/com/decoder135/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/TennisCoach.java
@@ -3,6 +3,6 @@ package com.decoder135.springcoredemo.common;
 public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return null;
+        return "Practice your backhand volley daily.";
     }
 }
