b2977adadb90313eb18bd79c44c15c966b1338ef
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 23:20:57 +0530

    add a return statement

--- src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
+++ src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
@@ -1,12 +1,13 @@
 package com.decoder135.springcoredemo.config;
 
 import com.decoder135.springcoredemo.common.Coach;
+import com.decoder135.springcoredemo.common.SwimCoach;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SportConfig {
 
     public Coach swimCoach() {
-
+        return new SwimCoach();
     }
 }
