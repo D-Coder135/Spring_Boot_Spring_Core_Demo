b1d19105a0f9da8744bcc90970bce83880292e2e
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 18:15:27 +0530

    add the @Scope annotation for prototype scope

--- src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
@@ -1,10 +1,13 @@
 package com.decoder135.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 // For Prototype scope of a bean, a new object instance is created for each injection.
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
