4101d5145d7d19eabb6f241a62e6b34cf9decef0
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 23:20:41 +0530

    add a public method swimCoach() of type Coach

--- src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
+++ src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
@@ -1,7 +1,12 @@
 package com.decoder135.springcoredemo.config;
 
+import com.decoder135.springcoredemo.common.Coach;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SportConfig {
+
+    public Coach swimCoach() {
+
+    }
 }
