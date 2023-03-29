960c5b0001fffd52518a7b22a5a8aff42e408fd7
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 23:04:58 +0530

    update the return statement

--- src/main/java/com/decoder135/springcoredemo/common/SwimCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/SwimCoach.java
@@ -3,6 +3,6 @@ package com.decoder135.springcoredemo.common;
 public class SwimCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return null;
+        return "Swim 1000 meters as a warm up";
     }
 }
