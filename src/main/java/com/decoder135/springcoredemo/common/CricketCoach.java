f86cc375c7106aa2302cd0ea2b2e1df531da0ac9
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 18:14:59 +0530

    add some comment inside the code

--- src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// For Prototype scope of a bean, a new object instance is created for each injection.
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
