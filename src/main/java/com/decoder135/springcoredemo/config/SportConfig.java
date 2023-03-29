5942af76a2f8bc66279fcfe013a565541b740530
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 23:21:08 +0530

    add the @Bean annotation

--- src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
+++ src/main/java/com/decoder135/springcoredemo/config/SportConfig.java
@@ -2,11 +2,13 @@ package com.decoder135.springcoredemo.config;
 
 import com.decoder135.springcoredemo.common.Coach;
 import com.decoder135.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SportConfig {
 
+    @Bean
     public Coach swimCoach() {
         return new SwimCoach();
     }
