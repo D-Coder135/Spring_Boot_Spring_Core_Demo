6374ecebd51f0f1bd824cddceef4313f476120b5
D-Coder135 <devansh1352000@gmail.com>
2023-03-29 18:27:41 +0530

    add some comment inside the code

--- src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/decoder135/springcoredemo/common/CricketCoach.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Component;
 // For Prototype scope of a bean, a new object instance is created for each injection.
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
+
+    // define our init method
+
+    // define our destroy method
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes daily.";
